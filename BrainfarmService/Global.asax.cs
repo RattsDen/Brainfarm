78d3973e8de06be646254c9a9643244eb1d20ebb
Eric Thompson <anyxius@gmail.com>
2017-05-26 20:58:01 -0400

    Implemented user session expiration

--- BrainfarmService/Global.asax.cs
+++ BrainfarmService/Global.asax.cs
@@ -2,16 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Caching;
 using System.Web.Security;
 using System.Web.SessionState;
 
 namespace BrainfarmService
 {
     // This is where you override certain events such as the service starting up or shutting down.
-    // Not much will need to be touched here.
 
     public class Global : System.Web.HttpApplication
     {
+        private const string CACHE_KEY_SESSION_CLEANUP = "UserSessionCleanup";
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            // Begin scheduling user session cleanups when the service starts
+            ScheduleUserSessionCleanup();
+        }
+
+        private void ScheduleUserSessionCleanup()
+        {
+            DateTime scheduledTime = DateTime.Now.AddMinutes(60); // Every 60 minutes
+
+            HttpRuntime.Cache.Insert(CACHE_KEY_SESSION_CLEANUP, scheduledTime, null, scheduledTime, 
+                Cache.NoSlidingExpiration, CacheItemPriority.NotRemovable, 
+                new CacheItemRemovedCallback(UserSessionCleanupDelegate));
+        }
+
+        public void UserSessionCleanupDelegate(string key, object value, CacheItemRemovedReason reason)
+        {
+            // Clean up expired user sessions
+            UserSessionManager.ClearExpiredSessions();
+            // Reschedule cleanup
+            ScheduleUserSessionCleanup();
+        }
+
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
             // When an http POST, PUT, or DELETE request is made to another domain via AJAX
--- BrainfarmService/UserSession.cs
+++ BrainfarmService/UserSession.cs
@@ -13,19 +13,32 @@ namespace BrainfarmService
         public DateTime RefreshTime { get; set; }
         public bool IsPersistent { get; set; }
 
+        private int sessionTimeToLive; // Minutes (since last refresh)
+
         public UserSession(User user, bool isPersistent)
         {
             User = user;
             StartTime = DateTime.Now;
             RefreshTime = StartTime;
             IsPersistent = isPersistent;
+
+            sessionTimeToLive = 60; // 60 minute time to live
         }
 
+        /// <summary>
+        /// Check if the user session has expired
+        /// </summary>
+        /// <returns>True if the user session has not yet expired, false if it has.</returns>
         public bool ValidateUserSession()
         {
-            return true;
+            // Session is still valid if it is persistent ("remember me" option)
+            //or if it has been less that sessionTimeToLive minutes since last refresh
+            return IsPersistent || RefreshTime.AddMinutes(sessionTimeToLive) > DateTime.Now;
         }
 
+        /// <summary>
+        /// Update the last refreshed time to the current system time.
+        /// </summary>
         public void Refresh()
         {
             RefreshTime = DateTime.Now;
