39a8b22f347567671892ad49cf0a82e3dd49ffd5
Eric Thompson <anyxius@gmail.com>
2017-10-03 20:05:12 -0400

    Removed debug text from CreateProject page

--- BrainfarmWeb/CreateProject.aspx.designer.cs
+++ BrainfarmWeb/CreateProject.aspx.designer.cs
@@ -56,14 +56,5 @@ namespace BrainfarmWeb {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblError;
-        
-        /// <summary>
-        /// debugText control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label debugText;
     }
 }
